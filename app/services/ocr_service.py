827ee38e2ec8192957377423c465644cf3856df0
Abhinav Sharma <abhinav.sharma_cs.aiml24@gla.ac.in>
2026-04-30 09:59:58 +0530

    Updated OCR services and deployment fixes

--- app/services/ocr_service.py
+++ app/services/ocr_service.py
@@ -2,7 +2,6 @@ import pytesseract
 import cv2
 import os
 
-# Set Tesseract path only for Windows
 if os.name == "nt":
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
@@ -13,7 +12,9 @@ def extract_text(image_path):
         raise ValueError(f"Image not found or unable to read: {image_path}")
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
     thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)[1]
 
     text = pytesseract.image_to_string(thresh)
+
     return text
