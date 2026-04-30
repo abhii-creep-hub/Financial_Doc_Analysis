7059f961217fcaaae6ce21b9cc68c7e10e784984
KARTIK <kartik.budhraja_cs.aiml24@gla.ac.in>
2026-05-01 05:04:12 +0530

    add google vision OCR

--- app/services/ocr_service.py
+++ app/services/ocr_service.py
@@ -1,8 +1,26 @@
+import json
+import os
+from google.cloud import vision
+from google.oauth2 import service_account
+import io
+
 def extract_text(image_path):
-    return """
-    Invoice No: INV-2024-001
-    Vendor: Amazon Pvt Ltd
-    Date: 12-05-2024
-    Total Amount: 5000
-    Tax: 500
-    """
+    try:
+        credentials_info = json.loads(os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"])
+        credentials = service_account.Credentials.from_service_account_info(credentials_info)
+
+        client = vision.ImageAnnotatorClient(credentials=credentials)
+
+        with io.open(image_path, 'rb') as image_file:
+            content = image_file.read()
+
+        image = vision.Image(content=content)
+        response = client.text_detection(image=image)
+
+        if response.text_annotations:
+            return response.text_annotations[0].description
+        else:
+            return "No text detected"
+
+    except Exception as e:
+        return "OCR failed"
