db5bc0928950a5992f8d3cc9d7a7d0a9548fa3dd
KARTIK <kartik.budhraja_cs.aiml24@gla.ac.in>
2026-05-01 05:42:59 +0530

    fix OCR API

--- app/services/ocr_service.py
+++ app/services/ocr_service.py
@@ -1,9 +1,13 @@
 import requests
+import os
 
-API_KEY = "K83791192288957"
+API_KEY = os.environ.get("OCR_API_KEY")
 
 def extract_text(image_path):
     try:
+        if not API_KEY:
+            return "ERROR: API key missing"
+
         with open(image_path, 'rb') as f:
             response = requests.post(
                 'https://api.ocr.space/parse/image',
@@ -16,11 +20,11 @@ def extract_text(image_path):
 
         result = response.json()
 
-        if result['IsErroredOnProcessing']:
-            return "OCR failed"
+        if result.get("IsErroredOnProcessing"):
+            return str(result)
 
         text = result['ParsedResults'][0]['ParsedText']
         return text.strip()
 
     except Exception as e:
-        return "OCR failed"
+        return str(e)
